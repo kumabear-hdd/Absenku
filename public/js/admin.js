5b464271b5e4a842193fe55a7e20f809a25eea04
kumabear-hdd <kumabear-hdd@users.noreply.github.com>
2026-07-31 15:25:43 +0700

    Reduce class list from 21 to 6 classes (5A-5F)

--- public/js/admin.js
+++ public/js/admin.js
@@ -34,7 +34,7 @@ async function loadDashboard() {
       }
     });
     const kelasTerpakai = kelasSet.size;
-    const semuaKelas = ['1A','1B','1C','2A','2B','2C','3A','3B','3C','4A','4B','4C','5A','5B','5C','5D','5E','5F','6A','6B','6C'];
+    const semuaKelas = ['5A','5B','5C','5D','5E','5F'];
     const kelasKosong = semuaKelas.filter(k => !kelasSet.has(k)).length;
 
     document.getElementById('statTotalWali').textContent = totalWali;
